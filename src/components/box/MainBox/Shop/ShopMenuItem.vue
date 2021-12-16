afd6d12dc870c05b0e12dce46d978c61df373bf2
Hi_old <hi_old@aliyun.com>
2021-12-16 14:14:54 +0800

    抽组件

--- src/components/box/MainBox/Shop/ShopMenuItem.vue
+++ src/components/box/MainBox/Shop/ShopMenuItem.vue
@@ -1,18 +1,21 @@
 <template>
-    <template>
-        <ul class="btn-1">
-            <div>活动折扣</div>
-        </ul>
-        <li class="b1-t0">全部</li>
-    </template>
+  <div class="visible">
+    <ul :class="['btn'+(menuindex+1%6)]">
+      <div>{{ menucategory }}</div>
+    </ul>
+    <li :class="['b'+(menuindex+1)+'-t'+(idx%6)]" v-for="(item,idx) in menuitem" :key="item.name">{{ item.name }}</li>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "ShopMenuItem"
-    }
+export default {
+  name: "ShopMenuItem",
+  props: ["menuitem", 'menucategory', 'menuindex']
+}
 </script>
 
 <style scoped>
-
+.visible {
+  overflow: visible;
+}
 </style>
