0b1e1d5ef4b6456385ed41dd0fb127c3a90adcbe
Hi_old <hi_old@aliyun.com>
2021-12-24 11:51:07 +0800

    调整vue3版本

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
 <template>
-  <router-view></router-view>
+  <router-view />
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-  components: {}
-}
+export default {};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/css/main.css";
+@import "./assets/css/color-dark.css";
 </style>
