1f6ca5ac0900fc10ecff1a3494bb18b0ff52c9dd
Hi_old <hi_old@aliyun.com>
2021-12-27 15:57:01 +0800

    本地代理、输入框联想搜索功能实现

--- src/views/tradeManageSystem/ShopManage.vue
+++ src/views/tradeManageSystem/ShopManage.vue
@@ -1,237 +1,190 @@
 <template>
-    <div>
-        <div class="crumbs">
-            <el-breadcrumb separator="/">
-                <el-breadcrumb-item>
-                    <i class="el-icon-lx-cascades"></i> 基础表格
-                </el-breadcrumb-item>
-            </el-breadcrumb>
-        </div>
-        <div class="container">
-            <div class="handle-box">
-                <el-select v-model="query.address" placeholder="地址" class="handle-select mr10">
-                    <el-option key="1" label="广东省" value="广东省"></el-option>
-                    <el-option key="2" label="湖南省" value="湖南省"></el-option>
-                </el-select>
-                <el-input v-model="query.name" placeholder="用户名" class="handle-input mr10"></el-input>
-                <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
-                <el-button type="success" icon="el-icon-plus" @click="handleAdd">添加</el-button>
-            </div>
-            <el-table :data="tableData" border class="table" ref="multipleTable" header-cell-class-name="table-header">
-                <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
-                <el-table-column prop="name" label="用户名"></el-table-column>
-                <el-table-column label="账户余额">
-                    <template #default="scope">￥{{ scope.row.money }}</template>
-                </el-table-column>
-                <el-table-column label="头像(查看大图)" align="center">
-                    <template #default="scope">
-                        <el-image class="table-td-thumb" :src="scope.row.thumb" :preview-src-list="[scope.row.thumb]">
-                        </el-image>
-                    </template>
-                </el-table-column>
-                <el-table-column prop="address" label="地址"></el-table-column>
-                <el-table-column label="状态" align="center">
-                    <template #default="scope">
-                        <el-tag :type="
-                                scope.row.state === '成功'
-                                    ? 'success'
-                                    : scope.row.state === '失败'
-                                    ? 'danger'
-                                    : ''
-                            ">{{ scope.row.state }}
-                        </el-tag>
-                    </template>
-                </el-table-column>
+  <div>
+    <div class="crumbs">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item>
+          <i class="el-icon-lx-cascades"></i> 基础表格
+        </el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="container">
+      <div class="handle-box">
+        <el-select placeholder="地址" class="handle-select mr10">
+          <el-option key="1" label="广东省" value="广东省"></el-option>
+          <el-option key="2" label="湖南省" value="湖南省"></el-option>
+        </el-select>
+        <el-input placeholder="用户名" class="handle-input mr10"></el-input>
+        <el-button type="primary" icon="el-icon-search" @click="">搜索</el-button>
+        <el-button type="success" icon="el-icon-plus" @click="handleAdd">添加</el-button>
+      </div>
+      <el-table border class="table" ref="multipleTable" header-cell-class-name="table-header">
+        <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
+        <el-table-column prop="name" label="用户名"></el-table-column>
+        <el-table-column label="账户余额">
+          <template #default="scope">￥</template>
+        </el-table-column>
+        <el-table-column label="头像(查看大图)" align="center">
+          <template #default="scope">
+            <el-image class="table-td-thumb" :src="scope.row.thumb" :preview-src-list="[scope.row.thumb]">
+            </el-image>
+          </template>
+        </el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+        <el-table-column label="状态" align="center">
+          <template #default="scope">
 
-                <el-table-column prop="date" label="注册时间"></el-table-column>
-                <el-table-column label="操作" width="180" align="center">
-                    <template #default="scope">
-                        <el-button type="text" icon="el-icon-edit" @click="handleEdit(scope.$index, scope.row)">编辑
-                        </el-button>
-                        <el-button type="text" icon="el-icon-delete" class="red"
-                                   @click="handleDelete(scope.$index, scope.row)">删除
-                        </el-button>
-                    </template>
-                </el-table-column>
-            </el-table>
-            <div class="pagination">
-                <el-pagination background layout="total, prev, pager, next" :current-page="query.pageIndex"
-                               :page-size="query.pageSize" :total="pageTotal"
-                               @current-change="handlePageChange"></el-pagination>
-            </div>
-        </div>
+          </template>
+        </el-table-column>
 
-        <!-- 编辑弹出框 -->
-        <el-dialog title="编辑" v-model="editVisible" width="30%">
-            <el-form label-width="70px">
-                <el-form-item label="用户名">
-                    <el-input v-model="form.name"></el-input>
-                </el-form-item>
-                <el-form-item label="地址">
-                    <el-input v-model="form.address"></el-input>
-                </el-form-item>
-            </el-form>
-            <template #footer>
-                <span class="dialog-footer">
-                    <el-button @click="editVisible = false">取 消</el-button>
-                    <el-button type="primary" @click="saveEdit">确 定</el-button>
-                </span>
-            </template>
-        </el-dialog>
-
-
-        <!-- 编辑弹出框 -->
-        <el-dialog title="新增" v-model="addVisible" width="30%">
-            <el-form label-width="70px">
-                <el-form-item label="用户名">
-                    <el-input v-model="form.name"></el-input>
-                </el-form-item>
-                <el-form-item label="地址">
-                    <el-input v-model="form.address"></el-input>
-                </el-form-item>
-            </el-form>
-            <template #footer>
-                <span class="dialog-footer">
-                    <el-button @click="addVisible = false">取 消</el-button>
-                    <el-button type="primary" @click="saveEdit">确 定</el-button>
-                </span>
-            </template>
-        </el-dialog>
+        <el-table-column prop="date" label="注册时间"></el-table-column>
+        <el-table-column label="操作" width="180" align="center">
+          <template #default="scope">
+            <el-button type="text" icon="el-icon-edit">编辑
+            </el-button>
+            <el-button type="text" icon="el-icon-delete" class="red"
+                       @click="">删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
 
     </div>
+
+
+    <!-- 编辑弹出框 -->
+    <el-dialog title="新增" v-model="addVisible" width="30%">
+      <el-form>
+        <!--物品种类-->
+        <el-form-item class="center">
+          <el-select v-model="itemType" placeholder="物品类型" class="handle-space">
+            <el-option key="1" label="游戏内物品" value="1"></el-option>
+            <el-option key="2" label="特殊物品" value="2"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item class="center">
+          <el-autocomplete :minlength="2" v-model="itemName" :fetch-suggestions="querySearchAsync" placeholder="请输入内容"
+                           @select="handleSelect"></el-autocomplete>
+        </el-form-item>
+
+      </el-form>
+      <template #footer>
+                <span class="dialog-footer">
+                    <el-button @click="addVisible = false">取 消</el-button>
+                    <el-button type="primary">确 定</el-button>
+                </span>
+      </template>
+    </el-dialog>
+
+  </div>
 </template>
 
 <script>
-    import {reactive, ref} from "vue";
-    import {fetchData} from "../../api";
-    import {ElMessage, ElMessageBox} from "element-plus";
+import {reactive, ref} from "vue";
+import {fetchData} from "../../api";
+import {ElMessage, ElMessageBox} from "element-plus";
+import axios from "axios";
 
-    export default {
-        name: "ShopManage",
-        setup() {
-            const query = reactive({
-                address: "",
-                name: "",
-                pageIndex: 1,
-                pageSize: 10,
-            });
-            const tableData = ref([]);
-            const pageTotal = ref(0);
-            // 获取表格数据
-            const getData = () => {
-                fetchData(query).then((res) => {
-                    tableData.value = res.list;
-                    pageTotal.value = res.pageTotal || 50;
-                });
-            };
-            getData();
+export default {
+  name: "ShopManage",
+  setup() {
+    axios.get('proxy/api/getSystemItem')
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
 
-            // 查询操作
-            const handleSearch = () => {
-                query.pageIndex = 1;
-                getData();
-            };
-            // 分页导航
-            const handlePageChange = (val) => {
-                query.pageIndex = val;
-                getData();
-            };
+    //当前选择的物品类型
+    var itemType = ref("");
+    var itemName = ref("");
+    /**
+     * 建立保存新增的窗口
+     */
+    const addVisible = ref(false);
 
-            // 删除操作
-            const handleDelete = (index) => {
-                // 二次确认删除
-                ElMessageBox.confirm("确定要删除吗？", "提示", {
-                    type: "warning",
-                })
-                    .then(() => {
-                        ElMessage.success("删除成功");
-                        tableData.value.splice(index, 1);
-                    })
-                    .catch(() => {
-                    });
-            };
+    const handleAdd = () => {
+      addVisible.value = true;
+    };
 
-            // 表格编辑时弹窗和保存
-            const editVisible = ref(false);
-            let form = reactive({
-                name: "",
-                address: "",
-            });
-            let idx = -1;
-            const handleEdit = (index, row) => {
-                idx = index;
-                Object.keys(form).forEach((item) => {
-                    form[item] = row[item];
-                });
-                editVisible.value = true;
-            };
-
-            /**
-             * 建立保存新增的窗口
-             */
-            const addVisible = ref(false);
-
-            const handleAdd = () => {
-                addVisible.value = true;
-            };
-
-            const saveEdit = () => {
-                editVisible.value = false;
-                ElMessage.success(`修改第 ${idx + 1} 行成功`);
-                Object.keys(form).forEach((item) => {
-                    tableData.value[idx][item] = form[item];
-                });
-            };
-
-            return {
-                query,
-                tableData,
-                pageTotal,
-                editVisible,
-                addVisible,
-                form,
-                handleSearch,
-                handlePageChange,
-                handleDelete,
-                handleEdit,
-                saveEdit,
-                handleAdd,
-            };
-        }
+    //执行查询
+    const querySearchAsync = (queryString, cb) => {
+      if (queryString && queryString.length >= 1) {
+        let params = {itemname: queryString}
+        axios.get("proxy/api/getSystemItem", {params}).then(res => {
+          if (res.data.respCode === "1") {
+            var JsonData = JSON.parse(res.data.data);
+            //处理返回结果
+            //声明一个数组存储待选项
+            var sugestion = [];
+            for (var i = 0; i < JsonData.length; i++) {
+              sugestion[i] = {value: JsonData[i].itemname};
+            }
+            console.log(sugestion)
+            cb(sugestion);
+          }
+        })
+      } else {
+        cb([]);
+      }
     }
+    const handleSelect = (item) => {
+      console.log(itemName, item);
+    }
+
+
+    return {
+      addVisible,
+      handleAdd,
+      itemType,
+      itemName,
+      querySearchAsync,
+      handleSelect,
+    };
+  }
+}
 </script>
 
 <style scoped>
-    .handle-box {
-        margin-bottom: 20px;
-    }
+.center {
+  text-align: center;
+}
 
-    .handle-select {
-        width: 120px;
-    }
+.handle-space {
+  width: 70%;
+}
 
-    .handle-input {
-        width: 300px;
-        display: inline-block;
-    }
+.handle-box {
+  margin-bottom: 20px;
+}
 
-    .table {
-        width: 100%;
-        font-size: 14px;
-    }
+.handle-select {
+  width: 120px;
+}
 
-    .red {
-        color: #ff0000;
-    }
+.handle-input {
+  width: 300px;
+  display: inline-block;
+}
 
-    .mr10 {
-        margin-right: 10px;
-    }
+.table {
+  width: 100%;
+  font-size: 14px;
+}
 
-    .table-td-thumb {
-        display: block;
-        margin: auto;
-        width: 40px;
-        height: 40px;
-    }
+.red {
+  color: #ff0000;
+}
+
+.mr10 {
+  margin-right: 10px;
+}
+
+.table-td-thumb {
+  display: block;
+  margin: auto;
+  width: 40px;
+  height: 40px;
+}
 </style>
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,20 @@ export default {
     plugins: [vue()],
     optimizeDeps: {
         include: ['schart.js']
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 8088,
+        // cors: true, //默认启用并允许任何源
+        https: false,
+        // open: true, //在服务器启动时自动在浏览器中打开应用程序
+        // 反向代理配置，注意rewrite写法
+        proxy: {
+            '/proxy': {
+                target: 'http://localhost:26911/', //代理接口
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/proxy/, '')
+            }
+        }
     }
 }
