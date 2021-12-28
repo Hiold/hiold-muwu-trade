dd806672d09b884685e2a49e1947d6c0b6d7bb20
Hi_old <hi_old@aliyun.com>
2021-12-28 17:42:15 +0800

    输入框

--- src/views/tradeManageSystem/ShopManage.vue
+++ src/views/tradeManageSystem/ShopManage.vue
@@ -52,7 +52,7 @@
 
 
     <!-- 编辑弹出框 -->
-    <el-dialog title="新增" v-model="addVisible" width="30%">
+    <el-dialog title="新增" v-model="addVisible" width="40%">
       <el-form>
         <!--物品种类-->
         <el-form-item class="center">
@@ -62,10 +62,70 @@
           </el-select>
         </el-form-item>
 
-        <el-form-item class="center">
-          <el-autocomplete :minlength="2" v-model="itemName" :fetch-suggestions="querySearchAsync" placeholder="请输入内容"
+        <el-form-item class="center" v-show="itemType==='1'">
+          <el-autocomplete style="width: 70%;" :minlength="2" v-model="itemName" :fetch-suggestions="querySearchAsync"
+                           placeholder="请输入内容"
                            @select="handleSelect"></el-autocomplete>
         </el-form-item>
+
+        <el-form-item class="center" v-if="itemType==='1'&&itemName!==''&&itemName!==null">
+          <el-image v-if="itemType==='1'&&itemName!==''&&itemName!==null"
+                    style="width: 100px; height: 100px"
+                    :src="src"
+                    fit="fit"
+          ></el-image>
+        </el-form-item>
+
+        <el-form-item class="center" v-if="itemType==='1'">
+          <el-tag>品质</el-tag>
+          <el-slider v-if="itemType==='1'" v-model="quality" :show-tooltip="true"
+                     step="1" max="6" show-stops show-input
+                     :marks="marks"></el-slider>
+        </el-form-item>
+        <br/>
+        <el-form-item class="center" v-if="itemType!==''">
+          <el-input class="handle-space" placeholder="数量" v-model="itemnum"></el-input>
+        </el-form-item>
+
+        <el-form-item class="center" v-if="itemType!==''">
+          <el-select v-model="currency" placeholder="货币种类" class="handle-space">
+            <el-option key="1" label="积分" value="1"></el-option>
+            <el-option key="2" label="钻石" value="2"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item class="center" v-if="itemType!==''">
+          <el-input class="handle-space" placeholder="数量" v-model="itemnum"></el-input>
+        </el-form-item>
+
+        <el-form-item class="center" v-if="itemType!==''">
+          <el-input class="handle-space" placeholder="价格" v-model="price"></el-input>
+        </el-form-item>
+
+        <el-form-item class="center" v-if="itemType!==''">
+          <el-tag>折扣</el-tag>
+          <el-slider v-model="discount" :show-tooltip="true"
+                     step="0.1" max="10" show-input show-tooltip></el-slider>
+        </el-form-item>
+
+        <el-form-item class="center" v-if="itemType!==''">
+          <el-input class="handle-space" placeholder="折后价格" v-model="prefer"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <div>
+            <div class="container">
+              <div class="plugins-tips">
+                wangEditor：轻量级 web 富文本编辑器，配置方便，使用简单。
+                访问地址：
+                <a href="https://www.wangeditor.com/doc/" target="_blank">wangEditor</a>
+              </div>
+              <div class="mgb20" id='editorHtml'></div>
+              <el-button type="primary" @click="syncHTML">提交</el-button>
+            </div>
+          </div>
+        </el-form-item>
+
 
       </el-form>
       <template #footer>
@@ -84,32 +144,78 @@ import {reactive, ref} from "vue";
 import {fetchData} from "../../api";
 import {ElMessage, ElMessageBox} from "element-plus";
 import axios from "axios";
+import WangEditor from "wangEditor";
 
 export default {
+  mounted() {
+    this.instance = new WangEditor('#editorHtml');
+    this.instance.config.zIndex = 1;
+    this.instance.create();
+  },
   name: "ShopManage",
-  setup() {
-    axios.get('proxy/api/getSystemItem')
-        .then(function (response) {
-          console.log(response);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-
-    //当前选择的物品类型
-    var itemType = ref("");
-    var itemName = ref("");
-    /**
-     * 建立保存新增的窗口
-     */
-    const addVisible = ref(false);
-
-    const handleAdd = () => {
-      addVisible.value = true;
-    };
-
-    //执行查询
-    const querySearchAsync = (queryString, cb) => {
+  data() {
+    return {
+      instance: null,
+      editor: null,
+      html: "",
+      text: "",
+      src: '',
+      prefer: "",
+      discount: "",
+      itemType: "",
+      currency: "",
+      price: "",
+      itemName: "",
+      itemnum: "",
+      addVisible: false,
+      itemNameCache: [],
+      quality: 0,
+      marks: {
+        0: {
+          style: {
+            color: '#1989FA',
+          },
+          label: '无品质',
+        },
+        1: {
+          style: {
+            color: 'rgb(157, 138, 106)',
+          },
+          label: '1品质',
+        }, 2: {
+          style: {
+            color: 'rgb(208, 129, 43)',
+          },
+          label: '2品质',
+        }, 3: {
+          style: {
+            color: 'rgb(163, 165, 28)',
+          },
+          label: '3品质',
+        }, 4: {
+          style: {
+            color: 'rgb(69, 195, 54)',
+          },
+          label: '4品质',
+        }, 5: {
+          style: {
+            color: 'rgb(51, 96, 207)',
+          },
+          label: '5品质',
+        }, 6: {
+          style: {
+            color: 'rgb(165, 44, 205)',
+          },
+          label: '6',
+        }
+      }
+    }
+  },
+  methods: {
+    handleAdd() {
+      this.addVisible = true;
+    },
+    querySearchAsync(queryString, cb) {
       if (queryString && queryString.length >= 1) {
         let params = {itemname: queryString};
         axios.get("proxy/api/getSystemItem", {params}).then(res => {
@@ -119,31 +225,38 @@ export default {
             //声明一个数组存储待选项
             let sugestion = [];
             for (var i = 0; i < JsonData.length; i++) {
-              sugestion[i] = {value: JsonData[i].itemname};
+              console.log()
+              sugestion[i] = {value: JsonData[i].translate[16]};
+              this.itemNameCache[JsonData[i].translate[16]] = JsonData[i].itemname;
             }
             cb(sugestion);
           }
         });
       }
-    }
-    const handleSelect = (item) => {
-      console.log(itemName, item);
+    },
+    //执行查询
+    handleSelect(item) {
+      // console.log(item)
+      this.itemName = item.value;
+      // console.log(itemNameCache[itemName.value], itemName, item);
+      this.src = 'proxy/api/image/' + this.itemNameCache[this.itemName] + '.png';
     }
 
-
-    return {
-      addVisible,
-      handleAdd,
-      itemType,
-      itemName,
-      querySearchAsync,
-      handleSelect,
-    };
   }
 }
 </script>
 
 <style scoped>
+.demonstration {
+  font-size: 14px;
+  /*color: var(--el-text-color-secondary);*/
+  line-height: 44px;
+  flex: 1;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
 .center {
   text-align: center;
 }
